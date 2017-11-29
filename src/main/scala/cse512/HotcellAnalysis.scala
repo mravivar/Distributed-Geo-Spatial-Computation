56fd4ba5da46ae2d6f96fa13a2b6ae57bcf7ce2b
aravindGear4 <athanga1@asu.edu>
2017-11-29 01:40:54 -0700

    orderby

--- src/main/scala/cse512/HotcellAnalysis.scala
+++ src/main/scala/cse512/HotcellAnalysis.scala
@@ -22,6 +22,16 @@ object HotcellAnalysis {
   var mean=0.0
   var sd=0.0
   var numCells=0.0
+  val minX = -74.50/HotcellUtils.coordinateStep
+  val maxX = -73.70/HotcellUtils.coordinateStep
+  val minY = 40.50/HotcellUtils.coordinateStep
+  val maxY = 40.90/HotcellUtils.coordinateStep
+  val minZ = 1
+  val maxZ = 31
+  val NEIGHBORS_ON_CORNERS = 7//8
+  val NEIGHBORS_ON_EDGE = 11//12
+  val NEIGHBORS_ON_FACE = 17//18
+  val NEIGHBORS_ON_INSIDE = 26//27
 def runHotcellAnalysis(spark: SparkSession, pointPath: String): DataFrame =
 {
   // Load the original data from a data source
@@ -42,15 +52,10 @@ def runHotcellAnalysis(spark: SparkSession, pointPath: String): DataFrame =
     )))
   pickupInfo = spark.sql("select CalculateX(nyctaxitrips._c5),CalculateY(nyctaxitrips._c5), CalculateZ(nyctaxitrips._c1) from nyctaxitrips")
   var newCoordinateName = Seq("x", "y", "z")
-  pickupInfo = pickupInfo.toDF(newCoordinateName:_*)
+  pickupInfo = pickupInfo.toDF(newCoordinateName:_*).persist()
 
   // Define the min and max of x, y, z
-  val minX = -74.50/HotcellUtils.coordinateStep
-  val maxX = -73.70/HotcellUtils.coordinateStep
-  val minY = 40.50/HotcellUtils.coordinateStep
-  val maxY = 40.90/HotcellUtils.coordinateStep
-  val minZ = 1
-  val maxZ = 31
+
   numCells = (maxX - minX + 1)*(maxY - minY + 1)*(maxZ - minZ + 1)
 
   // YOU NEED TO CHANGE THIS PART
@@ -60,7 +65,7 @@ def runHotcellAnalysis(spark: SparkSession, pointPath: String): DataFrame =
     .orderBy("x", "y", "z")
   println(pickupInfo.count())
   //groupby
-  pickupInfo = pickupInfo.groupBy("x", "y", "z").count()
+  pickupInfo=pickupInfo.groupBy("x", "y", "z").count().persist()
   mean = pickupInfo.agg(avg(pickupInfo("count"))).first().get(0).asInstanceOf[Double].doubleValue()
   sd = pickupInfo.agg(stddev(pickupInfo("count"))).first().get(0).asInstanceOf[Double].doubleValue()
 
@@ -69,15 +74,17 @@ def runHotcellAnalysis(spark: SparkSession, pointPath: String): DataFrame =
   var joinedResult = spark.sql("select t1.x as t1x,t1.y as t1y,t1.z as t1z,t2.x as t2x,t2.y as t2y,t2.z as t2z,t2.count as t2count from pickupinfo as t1 cross join pickupinfo as t2 " +
     "where (t1.x==t2.x or t1.x==t2.x-1 or t1.x==t2.x+1) and (t1.y==t2.y or t1.y==t2.y-1 or t1.y==t2.y+1) and (t1.z==t2.z or t1.z==t2.z-1 or t1.z==t2.z+1)")
 
-  joinedResult = joinedResult.groupBy(joinedResult("t1x"), joinedResult("t1y"), joinedResult("t1z")).agg(count("t2count"),sum(joinedResult("t2count")))
-  joinedResult = joinedResult.withColumnRenamed("count(t2count)", "neighbours")
+  joinedResult = joinedResult.groupBy(joinedResult("t1x"), joinedResult("t1y"), joinedResult("t1z")).agg(sum(joinedResult("t2count")))
   joinedResult = joinedResult.withColumnRenamed("sum(t2count)", "weight")
-  joinedResult.show()
+
+  spark.udf.register("countNeighbours", countNeighbours _)
+  joinedResult.createOrReplaceTempView("withoutNeighbours")
+  joinedResult = spark.sql("select t1x,t1y,t1z,weight,countNeighbours(t1x,t1y,t1z) as neighbours from withoutNeighbours")
   joinedResult.createOrReplaceTempView("joinedResult")
   spark.udf.register("zscore",(neighbours:Int, weight:Int)=>(zscore(neighbours, weight)))
-  var final_res = spark.sql("select t1x as x, t1y as y, t1z as z, zscore(neighbours, weight) as z_score from joinedResult").orderBy(desc("z_score"))
+  var final_res = spark.sql("select t1x as x, t1y as y, t1z as z, zscore(neighbours, weight) as z_score from joinedResult").orderBy(desc("z_score")).limit(50)
+  final_res.show(50)
   final_res = final_res.drop("z_score")
-  final_res.show()
   /*
 
     val list = pickupInfo.collectAsList()
@@ -140,6 +147,32 @@ def runHotcellAnalysis(spark: SparkSession, pointPath: String): DataFrame =
   return final_res // YOU NEED TO CHANGE THIS PART
 }
   def zscore(neighbours: Int, sum:Int): Double ={
-    return (sum - (mean * neighbours))/ (sd * Math.sqrt((numCells* neighbours - neighbours*neighbours)/ (numCells-1)))
+    val nr = sum- (mean* neighbours)
+    val dr = sd * Math.sqrt((numCells* neighbours - neighbours*neighbours)/ (numCells-1))
+    return nr/dr
+  }
+
+  def countNeighbours(x:Int, y:Int, z:Int): Int={
+    var terminalIndex=0
+    if(x==minX || x==maxX){
+      terminalIndex+=1
+    }
+    if(y==minY || y==maxY){
+      terminalIndex+=1
+    }
+    if(z==minZ|| z==maxZ){
+      terminalIndex+=1
+    }
+    var neighbours=0
+    if(terminalIndex==1){
+      neighbours=NEIGHBORS_ON_FACE
+    }else if(terminalIndex==2){
+      neighbours=NEIGHBORS_ON_EDGE
+    }else if(terminalIndex==3){
+      neighbours=NEIGHBORS_ON_CORNERS
+    }else{
+      neighbours=NEIGHBORS_ON_INSIDE
+    }
+    return neighbours
   }
 }
